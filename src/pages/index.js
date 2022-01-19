44e37f96306eb48ef4712bce606d8554d6650f43
Ulban Adhikary <46319463+UpsilonAlpha@users.noreply.github.com>
2022-01-19 17:33:05 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ class GifTile extends React.Component
 }
 const IndexPage = () => {
   return (
-<main style={pageStyles}>
+<main style={pageStyles} height="100%">
   <div style={centerImage}>
     <img src={logo} width="100px" padding="0px"/>
   </div>
